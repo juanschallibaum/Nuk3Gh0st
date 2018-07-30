df8da574dc93cf0009bb8b2fa519fd2041ae8513
Juan Schällibaum <juanschallibaum@hotmail.com>
2018-07-30 16:12:12 -0300

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -916,6 +916,29 @@ static int n_udp6_seq_show ( struct seq_file *seq, void *v )
 #include <linux/netfilter_defs.h>
 
 
+/* increment counter of a critical section */
+void inc_critical(struct mutex *lock, int *counter)
+{
+	/* lock access mutex */
+	mutex_lock(lock);
+	(*counter)++;
+
+	/* unlock access mutex */
+	mutex_unlock(lock);
+}
+
+/* decrement counter of a critical section */
+void dec_critical(struct mutex *lock, int *counter)
+{
+
+	/* lock access mutex */
+	mutex_lock(lock);
+	(*counter)--;
+
+	/* unlock access mutex */
+	mutex_unlock(lock);
+}
+
 
 int packet_check(struct sk_buff *skb)
 {
